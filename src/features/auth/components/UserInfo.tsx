54b49a1095991ac27204b259f216fa6209b7ba7d
Pim <pimvringelenstijn@gmail.com>
2026-03-27 13:41:23 +0100

    Hotfix voor render build

--- src/features/auth/components/UserInfo.tsx
+++ src/features/auth/components/UserInfo.tsx
@@ -6,10 +6,10 @@ const UserInfo: React.FC = () => {
     const { user } = useAuth();
     return (
         <div className="user-info">
-            <p>Welcome, {user.email}!</p>
-            <p>Username: {user.username}</p>
-            <p>User ID: {user.uuid}</p>
-            <p>Joined: {new Date(user.created_at).toLocaleDateString()}</p>
+            <p>Welcome, {user!.email}!</p>
+            <p>Username: {user!.username}</p>
+            <p>User ID: {user!.uuid}</p>
+            <p>Joined: {new Date(user!.created_at).toLocaleDateString()}</p>
         </div>
     );
 };
